720fc6633aef7d8b81f36edc4a9fc7732f4f0af1
NikSkor <mr-zerx@yandex.ru>
2022-09-27 16:30:51 +0500

    config done

--- webpack.config.js
+++ webpack.config.js
@@ -9,11 +9,17 @@ module.exports = {
   mode,
   target,
   devtool,
-  entry: path.resolve(__dirname, 'src', 'index.js'),
+  devServer: {
+    port: 3030,
+    open: true,
+    hot: true,
+  },
+  entry: ['@babel/polyfill', path.resolve(__dirname, 'src', 'index.js')],
   output: {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
     filename: '[name].[contenthash].js',
+    assetModuleFilename: 'assets/[hash][ext]',
   },
   plugins: [new HtmlWebpackPlugin({
     template: path.resolve(__dirname, 'src', 'index.html')
@@ -29,11 +35,64 @@ module.exports = {
         loader: "html-loader",
       },
       {
-        test: /\.css$/i,
+        test: /\.(c|sa|sc)ss$/i,
         use: [
           devMode ? "style-loader" : MiniCssExtractPlugin.loader,
-          "css-loader"],
+          "css-loader",
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [require("postcss-preset-env")]
+              }
+            }
+          },
+          "sass-loader",],
       },
+      {
+        test: /\.woff2?$/i,
+        type: 'asset/resource',
+        generator: {
+          filename:'fonts/[name][ext]',
+        }
+      },
+      {
+        test: /\.(jpe?g|png|webp|gif|svg)$/i,
+        use: {
+          loader: 'image-webpack-loader',
+          options: {
+            mozjpeg: {
+              progressive: true,
+            },
+            // optipng.enabled: false will disable optipng
+            optipng: {
+              enabled: false,
+            },
+            pngquant: {
+              quality: [0.65, 0.90],
+              speed: 4
+            },
+            gifsicle: {
+              interlaced: false,
+            },
+            // the webp option will enable WEBP
+            webp: {
+              quality: 75
+            }
+          }
+        },
+        type: 'asset/resource',
+      },
+      {
+        test: /\.m?js$/i,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
     ],
   },
 }
