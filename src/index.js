bbc41033e5440deace35aa70e963d76c17407729
NikSkor <mr-zerx@yandex.ru>
2022-10-06 19:22:54 +0500

    avatar upload done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './index.html';
 import './index.scss';
+import {avatarController} from './modules/avatarController';
 import {choicesController} from './modules/choicesController';
 import {modalController} from './modules/modalController';
 import {selectController} from './modules/selectController';
@@ -68,6 +69,10 @@ const init = () => {
   });
 
   choicesController();
+  const crp = avatarController({
+    inputFile: '.avatar__input',
+    uploadResult: '.avatar__result',
+  });
 
   // const modalPerson = modalController({
   //   modal: '.modal_person',
