49101341afc299ba3ff5457195379b295d6e2e6f
NikSkor <mr-zerx@yandex.ru>
2022-10-06 22:57:55 +0500

    search done

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import {choicesController} from './modules/choicesController';
 import {getCategory} from './modules/getCategory';
 import {modalController} from './modules/modalController';
 import {renderList} from './modules/renderList';
+import {searchControl} from './modules/searchControl';
 import {selectController} from './modules/selectController';
 import {showPassword} from './modules/showPassword';
 
@@ -78,6 +79,7 @@ const init = () => {
 
   getCategory();
   renderList();
+  searchControl();
   // const modalPerson = modalController({
   //   modal: '.modal_person',
   //   btnOpen: '.service',
